bf146091adb64f54fa4af43ef868bebddd366217
PC <PC@DESKTOP-RKAL2O9>
2025-04-10 18:14:25 +0100

    Add project files.

---
+++ FirstWapp/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "john", "Jane", "Victoria", "Sebastian", "David" };
+
+            return Ok(studentNames);
+        }
+
+    }
+}
